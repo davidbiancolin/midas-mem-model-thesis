611752703ef2284d9866feead8199f590ca83de4
David Biancolin <david.biancolin@gmail.com>
2018-12-18 22:00:03 -0800

    Pull in FASED

--- graffle-figures.mk
+++ graffle-figures.mk
@@ -1,8 +1,16 @@
-graffle_figures := figures/memory-model-block-diagram.pdf \
+graffle_figures := figures/target-graph.pdf \
+	figures/mapped-simulator.pdf \
+	figures/memory-model-block-diagram.pdf \
 	figures/model-operation-1.pdf \
 	figures/model-operation-2.pdf \
 	figures/model-operation-3.pdf \
 	figures/model-operation-4.pdf
+
+figures/target-graph.pdf:midas-graphics/graffle/masters-target.graffle
+	omnigraffle-export -c target-graph midas-graphics/graffle/masters-target.graffle figures/target-graph.pdf
+
+figures/mapped-simulator.pdf:midas-graphics/graffle/masters-target.graffle
+	omnigraffle-export -c f1 midas-graphics/graffle/masters-target.graffle figures/mapped-simulator.pdf
 
 figures/memory-model-block-diagram.pdf:midas-graphics/graffle/memory-model-block-diagram.graffle
 	omnigraffle-export -c full midas-graphics/graffle/memory-model-block-diagram.graffle figures/memory-model-block-diagram.pdf
